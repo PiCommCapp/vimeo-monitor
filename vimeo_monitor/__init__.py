4595ba2f3f8a9530de971119bedf43f1ae44914f
PiCommCapp <dcapparelli@picomms.tv>
2025-09-15 12:27:15 +0100

    refactor

--- vimeo_monitor/__init__.py
+++
@@ -1,9 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version('vimeo-monitor')
-except PackageNotFoundError:
-    __version__ = '(local)'
-
-del PackageNotFoundError
-del version
